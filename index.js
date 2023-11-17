d9dfba47ebb0e94398e8970951d85d0eb15f53bd
Ali Raihan Nashrullah <aliraihann@gmail.com>
2023-11-17 14:08:26 +0700

    Upload Project

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +25,35 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+let tempShortUrl = {};
+
+app.post('/api/shorturl', function(req, res) {
+  let originalUrl = req.body['url'];
+  const formatPattern = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$/;
+  const startPattern = /^(https?:\/\/)?(www\.)?/;
+  const endPattern = /(\.[a-zA-Z]{2,})+$/;
+  let shorturlInNum = Math.floor(Math.random() * 1000) + 1;
+  if (startPattern.test(originalUrl) && endPattern.test(originalUrl)) {
+    res.json({
+        error: 'invalid url',
+      });
+    } else {
+      tempShortUrl[shorturlInNum]= originalUrl;
+      res.json({
+      original_url: originalUrl,
+      short_url: shorturlInNum,
+    });
+  }
+})
+
+app.get('/api/shorturl/:newurl', function(req, res) {
+  let shortenedUrl = req.params.newurl;
+  if (tempShortUrl.hasOwnProperty(shortenedUrl)) {
+      return res.redirect(tempShortUrl[shortenedUrl]);
+  } else {
+    res.json({
+      error: 'invalid url',
+    });
+  }
+})
